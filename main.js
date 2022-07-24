3ef4cae0ea69d005a9e7c896ef2c75e63ebc3a84
k8732yamazaki <k8732yamazaki@gmail.com>
2022-07-24 00:46:16 -0700

    Implement accordion button for #service

--- main.js
+++ main.js
@@ -2,9 +2,10 @@ const toggle_title = document.querySelectorAll(".toggle_title");
 
 toggle_title.forEach(function (toggle) {
     toggle.addEventListener('click', function () {
-        this.nextElementSibling.classList.toggle("open");
-        // const height = this.nextElementSibling.clientHeight;
-        // this.nextElementSibling.style.height = height + "px";
-        // console.log(height);
+        const dd = this.nextElementSibling;
+        dd.classList.toggle("open");
+        const height = dd.querySelector("ul").clientHeight;
+        console.log(height);
+        this.nextElementSibling.style.paddingBottom = height + "px";
     })
 })
