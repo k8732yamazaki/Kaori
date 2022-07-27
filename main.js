46010da965a8c23f4970fe21954d6ed634c9fc5a
k8732yamazaki <k8732yamazaki@gmail.com>
2022-07-26 21:36:37 -0700

    Implement hamburger menu for smartphone

--- main.js
+++ main.js
@@ -1,7 +1,20 @@
+// accordion buttons for #service .client on smartphone
 const toggle_title = document.querySelectorAll(".toggle_title");
 
 toggle_title.forEach(function (toggle) {
     toggle.addEventListener('click', function () {
         this.parentElement.classList.toggle("open");
-    })
-})
+    });
+});
+
+// hamburger menu on smartphone
+
+function toggleNav() {
+    var hamburger = document.getElementById("hamburger");
+    var header = document.getElementsByTagName("header");
+    
+    hamburger.addEventListener('click', function () {
+        header[0].classList.toggle("nav_open");
+    });
+};
+toggleNav();
