eba008e8ccb9779b1786d4859c508b9035a75bda
k8732yamazaki <k8732yamazaki@gmail.com>
2022-07-27 00:31:13 -0700

    Implement hamburger menu for smartphone

--- main.js
+++ main.js
@@ -4,17 +4,23 @@ const toggle_title = document.querySelectorAll(".toggle_title");
 toggle_title.forEach(function (toggle) {
     toggle.addEventListener('click', function () {
         this.parentElement.classList.toggle("open");
-    });
+    })
 });
 
 // hamburger menu on smartphone
 
 function toggleNav() {
-    var hamburger = document.getElementById("hamburger");
-    var header = document.getElementsByTagName("header");
+    const header = document.getElementsByTagName("header");
+    const hamburger = document.getElementById("hamburger");
+    const nav = header[0].querySelectorAll("nav");
     
     hamburger.addEventListener('click', function () {
         header[0].classList.toggle("nav_open");
-    });
+        nav.forEach(function (hamburgerMenu) {
+            hamburgerMenu.addEventListener('click', function () {
+                header[0].classList.remove("nav_open");
+            })
+        })
+    })
 };
 toggleNav();
