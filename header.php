126e39f17b9847e542fd1c1c6d66e4a942396611
k8732yamazaki <k8732yamazaki@gmail.com>
2022-08-02 20:43:18 -0700

    Set up php code for header

---
+++ header.php
@@ -0,0 +1,13 @@
+	<header>
+		<div class="inner">
+			<h1><img src="images/logo.png" alt="T&amp;Dビクトリアサポートセンターのロゴ画像"></h1>
+			<nav>
+				<ul>
+					<li><a href="#service">サービス内容</a></li>
+					<li><a href="#school">学校について</a></li>
+					<li><a href="#company">会社概要</a></li>
+					<li><a href="#contact">お問い合わせ</a></li>
+				</ul>
+			</nav>
+		</div>
+	</header>
