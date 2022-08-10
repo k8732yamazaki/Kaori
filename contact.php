c6b33704853c1cce5cb11bfffdf0879db93606ff
k8732yamazaki <k8732yamazaki@gmail.com>
2022-08-09 18:21:19 -0700

    Email link dummy text

--- contact.php
+++ contact.php
@@ -2,7 +2,7 @@
 			<div class="inner">
 				<h2>お問い合わせ</h2>
 				<p>メールにて受け付けております。<br>以下の宛先までご連絡ください。</p>
-				<a href="mailto:tdvsc&#64;shaw.ca"><p class="btn_contact">tdvsc&#64;shaw.ca</p></a>
+				<a href="mailto:tdvsc&#64;shaw.ca"><p class="btn_contact">tdvsc<span class="email_dummy">vGfokdxZJZnCadX0iI16TklBwdjyOeQt</span>&#64;<span class="email_dummy">vGfokdxZJZnCadX0iI16TklBwdjyOeQt</span>shaw<span class="email_dummy">vGfokdxZJZnCadX0iI16TklBwdjyOeQt</span>.ca</p></a>
 				<p class="copy">© 2005 T&amp;D Victoria Support Center</p>
 			</div>
 		</section>
