5a5d87d836417cc9038572e5d042f114e27d148e
k8732yamazaki <k8732yamazaki@gmail.com>
2022-08-10 12:25:06 -0700

    Modify all links into absolute paths

--- header.php
+++ header.php
@@ -1,12 +1,12 @@
 	<header>
 		<div class="inner">
-			<h1><a href="http://tdvsc.com/renewal_test/"><img src="http://tdvsc.com/renewal_test/images/logo.png" alt="T&amp;Dビクトリアサポートセンターのロゴ画像"></a></h1>
+			<h1><a href="http://tdvsc.com/"><img src="http://tdvsc.com/images/logo.png" alt="T&amp;Dビクトリアサポートセンターのロゴ画像"></a></h1>
 			<nav>
 				<ul>
-					<li><a href="http://tdvsc.com/renewal_test/#service">サービス内容</a></li>
-					<li><a href="http://tdvsc.com/renewal_test/#school">学校について</a></li>
-					<li><a href="http://tdvsc.com/renewal_test/#company">会社概要</a></li>
-					<li><a href="http://tdvsc.com/renewal_test/#contact">お問い合わせ</a></li>
+					<li><a href="http://tdvsc.com/#service">サービス内容</a></li>
+					<li><a href="http://tdvsc.com/#school">学校について</a></li>
+					<li><a href="http://tdvsc.com/#company">会社概要</a></li>
+					<li><a href="http://tdvsc.com/#contact">お問い合わせ</a></li>
 				</ul>
 			</nav>
 			<div id="hamburger">
