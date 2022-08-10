851e59c8abe802579a88bc2af2adc9024cbe994f
k8732yamazaki <k8732yamazaki@gmail.com>
2022-08-09 20:14:50 -0700

    Change link path

--- header.php
+++ header.php
@@ -1,12 +1,12 @@
 	<header>
 		<div class="inner">
-			<h1><img src="images/logo.png" alt="T&amp;Dビクトリアサポートセンターのロゴ画像"></h1>
+			<h1><a href="http://tdvsc.com/renewal_test/"><img src="http://tdvsc.com/renewal_test/images/logo.png" alt="T&amp;Dビクトリアサポートセンターのロゴ画像"></a></h1>
 			<nav>
 				<ul>
-					<li><a href="#service">サービス内容</a></li>
-					<li><a href="#school">学校について</a></li>
-					<li><a href="#company">会社概要</a></li>
-					<li><a href="#contact">お問い合わせ</a></li>
+					<li><a href="http://tdvsc.com/renewal_test/#service">サービス内容</a></li>
+					<li><a href="http://tdvsc.com/renewal_test/#school">学校について</a></li>
+					<li><a href="http://tdvsc.com/renewal_test/#company">会社概要</a></li>
+					<li><a href="http://tdvsc.com/renewal_test/#contact">お問い合わせ</a></li>
 				</ul>
 			</nav>
 			<div id="hamburger">
