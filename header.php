b861a96aa5457604307b9f70ef53b4f4987e34a4
k8732yamazaki <k8732yamazaki@gmail.com>
2022-08-10 12:29:30 -0700

    Change image directory name

--- header.php
+++ header.php
@@ -1,6 +1,6 @@
 	<header>
 		<div class="inner">
-			<h1><a href="http://tdvsc.com/"><img src="http://tdvsc.com/images/logo.png" alt="T&amp;Dビクトリアサポートセンターのロゴ画像"></a></h1>
+			<h1><a href="http://tdvsc.com/"><img src="http://tdvsc.com/image/logo.png" alt="T&amp;Dビクトリアサポートセンターのロゴ画像"></a></h1>
 			<nav>
 				<ul>
 					<li><a href="http://tdvsc.com/#service">サービス内容</a></li>
